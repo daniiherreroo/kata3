cc2e53a41f4fc67f4eaa9af8e4720367a7c1acf4
Entrar <Entrar@APD-L13>
2017-10-20 10:57:26 +0100

    Kata 3 Version 3 y 4: Convertimos HistogramDisplay en genérica, además modificamos el método increment() para poder pasarle el valor en el main.

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -4,16 +4,10 @@ public class Kata3 {
 
     public static void main(String[] args) {
         Histogram histo = new Histogram();
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ull.es");
-        histo.increment("ull.es");
-        histo.increment("hotmail.com");
+        histo.increment("gmail.com", 12);
+        histo.increment("ulpgc.es", 8);
+        histo.increment("ull.es", 2);
+        histo.increment("hotmail.com", 4);
         new HistogramDisplay(histo).execute();
     }
     
